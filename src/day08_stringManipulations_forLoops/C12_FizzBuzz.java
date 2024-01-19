039d9754862029dce72c1f7bc15359191f84bb01
bulutluoz <abbulutluoz@gmail.com>
2024-01-19 12:47:40 +0100

    8.gun sonu

---
+++ src/day08_stringManipulations_forLoops/C12_FizzBuzz.java
@@ -0,0 +1,32 @@
+package day08_stringManipulations_forLoops;
+
+import java.util.Scanner;
+
+public class C12_FizzBuzz {
+
+    public static void main(String[] args) {
+
+        //Kullanicidan pozitif bir sayi alin, 1’den baslayarak tum tamsayilari yazdirin, sira
+        //	- 3 ile bolunebilen bir sayiya gelirse, sayi yerine fizz
+        //	- 5 ile bolunebilen bir sayiya gelirse sayi yerine buzz
+        //	- hem 3 hem 5 ile bolunebilen bir sayiya gelirse sayi yerine fizzBuzz yazdirin
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen pozitif bir tamsayi giriniz...");
+        int girilensayi = scanner.nextInt();
+
+        for (int i = 1; i <=girilensayi ; i++) {
+
+            if (i%3 ==0 && i%5==0){
+                System.out.println("fizzBuzz ");
+            } else if (i%3 ==0) {
+                System.out.print("fizz ");
+            } else if (i%5==0) {
+                System.out.print("buzz ");
+            }else {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
