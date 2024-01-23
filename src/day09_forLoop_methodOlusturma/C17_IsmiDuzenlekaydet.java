dae69c5fa52afd45f78cc085e38d9dd7568ae985
bulutluoz <abbulutluoz@gmail.com>
2024-01-23 09:49:26 +0100

    10.gun sonu

--- src/day09_forLoop_methodOlusturma/C17_IsmiDuzenlekaydet.java
+++ src/day09_forLoop_methodOlusturma/C17_IsmiDuzenlekaydet.java
@@ -14,5 +14,37 @@ public class C17_IsmiDuzenlekaydet {
 
         // ismi duzenleyip kaydedelim
         String duzenlenmisHali = "";
+
+
+        ismiDuzenleKaydet(isim,soyisim,kkNo);
+        // 19.satirda method calisir ve duzenlenmis hali bize getirir
+        // bu duzenlenmis halini YA kaydetmeli, YA DA direk yazdirmaliyiz
+
+        System.out.println(ismiDuzenleKaydet(isim, soyisim, kkNo)); // A** C**, **** **** **** 6543
+
+
+        // duzenlenmis halindeki tum *'lari # yapin
+        duzenlenmisHali = ismiDuzenleKaydet(isim, soyisim, kkNo);
+        System.out.println(duzenlenmisHali); // A** C**, **** **** **** 6543
+
+        duzenlenmisHali = duzenlenmisHali.replace("*","#");
+
+        System.out.println(duzenlenmisHali); // A## C##, #### #### #### 6543
+
+
+    }
+
+
+    public static String ismiDuzenleKaydet(String isim, String soyisim, String kkNo){
+
+        String duzenlenmisHali= isim.toUpperCase().charAt(0) +
+                                isim.substring(1).replaceAll("\\w","*")+
+                                " "+
+                                soyisim.toUpperCase().charAt(0)+
+                                soyisim.substring(1).replaceAll("\\w","*")+
+                                ", **** **** **** " +
+                                kkNo.substring(kkNo.length()-4);
+        return duzenlenmisHali;
+
     }
 }
