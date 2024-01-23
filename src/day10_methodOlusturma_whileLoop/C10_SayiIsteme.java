92c3d19b2909111f420f997f885ebada9b4ea301
bulutluoz <abbulutluoz@gmail.com>
2024-01-23 12:28:31 +0100

    10.gun sonu

--- src/day10_methodOlusturma_whileLoop/C10_SayiIsteme.java
+++ src/day10_methodOlusturma_whileLoop/C10_SayiIsteme.java
@@ -1,5 +1,7 @@
 package day10_methodOlusturma_whileLoop;
 
+import java.util.Scanner;
+
 public class C10_SayiIsteme {
 
     public static void main(String[] args) {
@@ -9,8 +11,42 @@ public class C10_SayiIsteme {
         // "Guzel sayi" yazdirin ve islemi bitirin
         // buyuk degilse "Kotu sayi" yazdirin ve tekrar sayi girmesini isteyin
 
+        Scanner scanner = new Scanner(System.in);
+        int girilenSayi = 0;
+        boolean devamMi = true;
+        int ptbs = 0;
+
+        while (devamMi){ // devamMi == true
+
+            System.out.println("Pozitif bir tamsayi giriniz...");
+            girilenSayi = scanner.nextInt();
+
+            ptbs = pozitifTamBolenSayisiBul(girilenSayi);
+
+            if (ptbs>5){
+                System.out.println("Guzel sayi");
+                // break;
+                devamMi = false;
+            }else{
+                System.out.println("Kotu sayi");
+            }
+
+        }
 
 
 
+    }
+
+    public static int pozitifTamBolenSayisiBul(int sayi){
+
+        int sayac = 0;
+
+        for (int i = 1; i <=sayi ; i++) {
+
+            if (sayi % i == 0){
+                sayac++;
+            }
+        }
+        return sayac;
     }
 }
