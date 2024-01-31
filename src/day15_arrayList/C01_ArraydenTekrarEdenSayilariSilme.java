665712fb6bdb850e9334a76ef300f72f8177d461
bulutluoz <abbulutluoz@gmail.com>
2024-01-31 09:46:37 +0100

    15.gun sonu

--- src/day15_arrayList/C01_ArraydenTekrarEdenSayilariSilme.java
+++ src/day15_arrayList/C01_ArraydenTekrarEdenSayilariSilme.java
@@ -1,5 +1,9 @@
 package day15_arrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class C01_ArraydenTekrarEdenSayilariSilme {
 
     public static void main(String[] args) {
@@ -8,6 +12,31 @@ public class C01_ArraydenTekrarEdenSayilariSilme {
         // array'i unique sayilardan olusan hale donusturun
         // input :  {3,5,6,7,2,3,4,5,6,7,8,2,3,4,5,6,7};
         // output : {3,5,6,7,2,4,8}
+
+        int[] sayilar = {3,5,6,7,2,3,4,5,6,7,8,2,3,4,5,6,7};
+
+        List<Integer> tekrarsizList = new ArrayList<>();
+
+        for (int i = 0; i < sayilar.length ; i++) {
+
+            if (!tekrarsizList.contains(sayilar[i])){
+
+                tekrarsizList.add(sayilar[i]);
+            }
+        }
+
+        System.out.println("Tekrarsiz liste : " + tekrarsizList);
+        // Tekrarsiz liste : [3, 5, 6, 7, 2, 4, 8]
+
+
+        sayilar = new int[tekrarsizList.size()]; // [0, 0, 0, 0, 0, 0, 0]
+
+        for (int i = 0; i < sayilar.length; i++) {
+
+            sayilar[i] = tekrarsizList.get(i);
+        }
+
+        System.out.println("Sayilar array'inin son hali : " + Arrays.toString(sayilar));
 
 
     }
