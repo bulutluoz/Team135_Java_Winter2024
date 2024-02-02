b8379db39bf661d457ff199b0dfa793542e0395b
bulutluoz <abbulutluoz@gmail.com>
2024-02-02 18:13:10 +0100

    16.gun sonu

--- src/day16_foreachLoop_constructors/C06_Hastane.java
+++ src/day16_foreachLoop_constructors/C06_Hastane.java
@@ -2,7 +2,28 @@ package day16_foreachLoop_constructors;
 
 public class C06_Hastane {
 
-    // Kaliphane
+    /*
+        Java'nin class'lara otomatik olarak koydugu default constructor
+        parametresizdir ve body'sinde kod yoktur
+
+        default constructor'in aynisini
+        gozle gorunur olarak biz olusturursak
+        asagidaki gibi olur
+        ama gorunur oldugundan buna
+        default constructor DENILMEZ,
+
+        Gozle gorunur bir constructor varsa, java default constructor'i siler
+
+        Bir kod blogunun constructor olabilmesi icin
+        2 sarti MUTLAKA saglamalidir
+        1- ismi class ismi ile ayni olmalidir
+        2- returnType olmamalidir (method'larda olur)
+     */
+
+    public C06_Hastane(){
+        System.out.println("Bu parametresiz constructor'dir");
+        System.out.println("Default constructor gorunmez");
+    }
 
 
     static String hastaneIsmi = "Yildiz hastanesi";
@@ -12,5 +33,22 @@ public class C06_Hastane {
     String personelIsmi = "Isim atanmadi";
     String personelAdresi = "Adres belirtilmedi";
     int personelYas;
+
+    public static void Deneme(){
+        // method isimleri naming convention'a uygun olmasi icin kucuk harfle baslamalidir
+        // ancak buyuk harfle baslarsa da Java CTE vermez
+    }
+
+    public void C06_Hastane(){ // Method name 'C06_Hastane' is the same as its class name
+        // bu bir method'dur constructor degil
+        // cunku returnType'i var
+    }
+
+    //public DENEME(){ // Invalid method declaration; return type required
+        // ismi buyuk harfle yazilan ama class ismi ile ayni olmayan kod blogu
+        // constructor olamaz cunku ismi class ismi ile ayni degil
+        // method da OLAMAZ cunku return type yok
+        // Java CTE verir
+    //}
 
 }
--- src/day16_foreachLoop_constructors/C07_Runner.java
+++ src/day16_foreachLoop_constructors/C07_Runner.java
@@ -8,18 +8,49 @@ public class C07_Runner {
 
     public static void main(String[] args) {
 
-        // Biz bir class'dan obje olusturdugumuzda
-        // o obje olusturuldugu class'daki
-        // tum variable ve method'lara ulasabilir
+        /* Biz bir class'dan obje olusturdugumuzda
+           o obje olusturuldugu class'daki
+           tum variable ve method'lara ulasabilir
+
+           Hatta sadece olusturuldugu class'daki variable ve method'lara ulasmakla kalmaz
+           olustruruldugu class'da variable'lara atanan degerlere de sahip olur
+
+           Java'da bir obje olusturdugumuzda
+           objenin olusturuldugu class'daki Constructor calisir
+           ve olusturulan objeye ilk deger atamasini (initialize) yapar
+
+           C06_Hastane    pers1        =           new       C06_Hastane()         ;
+           Class Ismi   obje adi   assignment    keyword     Constructor
+           Data turu                 sign
+
+           non-primitive variable'larin data turu, olusturulduklari class'dir
+
+           C06_Hastane()  Hastane class'indaki parametresiz constructor'dir
+
+           Biz yeni bir class olusturdugumuzda bu constructor'i gormuyoruz
+
+           SEBEP :
+
+           Bir class'dan bir obje olusturulup,
+           ilk deger atamasinin yapilabilmesi icin CONSTRUCTOR'in calismasi ZORUNLUDUR
+           tersten soylersek, bir constructor calismadan bir objeye ilk deger atamasi YAPILAMAZ
+
+           Java bunu bildiginden
+           olusturulan her class'a OTOMATIK olarak default constructor koyar.
+           Biz kendi constructor'imizi olusturmak istersek olusturabiliriz
+           SADECE sunu bilmemiz lazim
+           biz bir gorunur constructor olusturdugumuzda
+           Java DEFAULT CONSTRUCTOR'I SILER
+         */
 
         List<String> liste = new ArrayList<>();
-        //liste.size();
+        liste.size();
 
         Scanner scanner = new Scanner(System.in);
         //scanner.next();
 
         String str = new String("");
-        //str.length();
+        str.length();
 
         C06_Hastane pers1 = new C06_Hastane();
 
