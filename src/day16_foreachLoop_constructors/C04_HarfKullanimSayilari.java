128cfc4390e24591352e6f43211f23c3a99bfff2
bulutluoz <abbulutluoz@gmail.com>
2024-02-02 11:14:02 +0100

    16.gun sonu

--- src/day16_foreachLoop_constructors/C04_HarfKullanimSayilari.java
+++ src/day16_foreachLoop_constructors/C04_HarfKullanimSayilari.java
@@ -1,5 +1,8 @@
 package day16_foreachLoop_constructors;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class C04_HarfKullanimSayilari {
 
     public static void main(String[] args) {
@@ -8,6 +11,29 @@ public class C04_HarfKullanimSayilari {
         //        harf cumlede kullanilmissa kac kere kullanildigini yazdirin,
         //        kullanilmadiysa “harf cumlede kullanilmamis” yazdirin.
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen bir cumle giriniz...");
+        String cumle = scanner.nextLine(); // Ali topu at,at Ali at
 
+        System.out.println("Lutfen bir harf giriniz...");
+        String harf = scanner.next(); // a
+
+        String[] harfler = cumle.split(""); // [A, l, i,  , t, o, p, u,  , a, t, ,, a, t,  , A, l, i,  , a, t]
+
+        int sayac = 0 ;
+
+        for ( String each : harfler
+             ) {
+
+            if (each.equalsIgnoreCase(harf)){
+                sayac++;
+            }
+        }
+
+        if (sayac == 0){
+            System.out.println("Verilen " + harf + " harfi, cumlede KULLANILMAMIS");
+        }else{
+            System.out.println("Verilen " + harf + " harfi, cumlede " + sayac + " kere kullanilmis");
+        }
     }
 }
