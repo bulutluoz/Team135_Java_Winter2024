7123ca0f70d2f03903212414efcf59094a51649a
bulutluoz <abbulutluoz@gmail.com>
2024-02-20 12:34:40 +0100

    day26 sonu

--- src/day26_pollymorphism_abstraction/C04_KuralciAbstractParent.java
+++ src/day26_pollymorphism_abstraction/C04_KuralciAbstractParent.java
@@ -1,5 +1,7 @@
 package day26_pollymorphism_abstraction;
 
+
+
 public abstract class C04_KuralciAbstractParent {
 
     /*
@@ -11,20 +13,40 @@ public abstract class C04_KuralciAbstractParent {
         child class'larin parent tarafindan belirlenen bazi method'lari
         override etme mecburiyeti olmasini saglamaktir.
 
-        Bunu saglamak icin
+        Abstract class'lar bir projede olmak zorunda degildir
+        Eger obje olusturmayacagimiz
+        ama child class'larin ortak ozelliklerini belirleyecek
+        class'lara ihtiyacimiz olursa Abstract class olustururuz
+
+        Abstract olmayan class ve method'lari belirtmek istedigimizde
+        abstract olmayan demek yerine concrete diyebiliriz
+
+
+        Bir class'in child class'lari
+        belirlenen method'lari override etmeye MECBUR BIRAKMASI icin
 
         1- Class'imizi abstract keyword kullanarak
            abstract class yapmaliyiz
 
-        2-
+        2- Child class'lari override etmeye mecbur birakacagimiz
+           method(lar)'i da abstract keyword kullanarak
+           abstract method yapmaliyiz
+
+        3- abstract(soyut) yapilan method'lar body'siz olurlar
+           bu method'lari su ana kadar kullandigimiz method'lar gibi degil
+           standartlari belirleyen bir kural cumlesi gibi dusunebiliriz
+
+           abstract method'larin body'si olmadigindan
+           abstract class'lardan obje olusturulamaz
+           bunun yerine abstract method'lara body kazandiran
+           child class'larin constructor'lari kullanilabilir
 
 
      */
 
     // bu class'i parent edinenler, method1'i kendilerine uyarlamak zorunda olsun
-    protected void method1(){
-        System.out.println("parent method1");
-    }
+    protected abstract void method1();
+    // 34.satir diyor ki : bu class'i child edinenler method1() sahibi olmak zorundadir
 
     protected void method2(){
         System.out.println("parent method2");
---
+++ src/day26_pollymorphism_abstraction/C05_ChildOfAbstractClass.java
@@ -0,0 +1,32 @@
+package day26_pollymorphism_abstraction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C05_ChildOfAbstractClass extends C04_KuralciAbstractParent{
+    // Class 'C05_ChildOfAbstractClass' must either be declared abstract
+    // or implement abstract method 'method1()' in 'C04_KuralciAbstractParent'
+
+
+    @Override
+    protected void method1() {
+
+    }
+
+    public static void main(String[] args) {
+
+        // C04_KuralciAbstractParent abj = new C04_KuralciAbstractParent();
+        // 'C04_KuralciAbstractParent' is abstract; cannot be instantiated
+
+
+        // Abstract class'larin constructor'lari vardir
+        // amma obje OLUSTURULAMAZ
+        // obje olusturmamiz gerektiginde
+        // constructor olarak child class constructor'lari kullanilabilir
+
+        C04_KuralciAbstractParent abj = new C05_ChildOfAbstractClass();
+        List<String> isimler = new ArrayList<>();
+    }
+
+
+}
