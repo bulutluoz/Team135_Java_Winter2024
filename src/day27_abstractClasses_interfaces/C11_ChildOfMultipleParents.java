08803e6f31c6592759f0e1600f73dbd79ad05266
bulutluoz <abbulutluoz@gmail.com>
2024-02-21 12:07:50 +0100

    day27 sonu

--- src/day27_abstractClasses_interfaces/C11_ChildOfMultipleParents.java
+++ src/day27_abstractClasses_interfaces/C11_ChildOfMultipleParents.java
@@ -9,7 +9,7 @@ public class C11_ChildOfMultipleParents
     public void method2() {
         // InterfaceParent1 "Mutlaka method2'in olmali"
         // InterfaceParent2 "Mutlaka method2'in olmali"
-
+        System.out.println("Child class method2");
     }
 
     @Override
@@ -18,5 +18,20 @@ public class C11_ChildOfMultipleParents
         return 5;
     }
 
+    public static void main(String[] args) {
 
+        C11_ChildOfMultipleParents obj = new C11_ChildOfMultipleParents();
+
+        obj.method1(); // Parent class method1
+        obj.method2(); // Child class method2
+        System.out.println(obj.method3()); // 5
+
+        // System.out.println(sayi); iki interface'de de sayi variable'i oldugundan CTE verir
+        System.out.println(C09_InterfaceParent1.sayi); // 20
+        System.out.println(C10_InterfaceParent2.sayi); // 30
+
+        System.out.println(isim); // Ali
+        System.out.println(yas); // 40
+
+    }
 }
