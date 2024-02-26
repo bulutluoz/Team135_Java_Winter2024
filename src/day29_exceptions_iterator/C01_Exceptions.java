7ab07935584713634d42335fd10707d9bf2aa684
bulutluoz <abbulutluoz@gmail.com>
2024-02-26 09:28:20 +0100

    day29 sonu

---
+++ src/day29_exceptions_iterator/C01_Exceptions.java
@@ -0,0 +1,46 @@
+package day29_exceptions_iterator;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class C01_Exceptions {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan toplanmak uzere sayilar isteyin
+        // kullanici sayi girisini durdurmak icin Q'ya basmalidir
+        // kullanici Q'ya bastiginda, toplam kac sayi girdigini
+        // ve girilen sayilarin toplamini yazdirin
+
+
+        Scanner scanner = new Scanner(System.in);
+        int girilenSayi=0;
+        int sayiAdedi = 0;
+        int sayilarinToplami = 0;
+        String girilenHarf = "a";
+
+        while(!girilenHarf.equalsIgnoreCase("Q")){
+
+            System.out.println("Toplanmak uzere tam sayi giriniz \nbitirmek icin Q'ya basin");
+
+            try {
+                girilenSayi = scanner.nextInt();
+                sayilarinToplami += girilenSayi;
+                sayiAdedi++;
+            } catch (InputMismatchException e) {
+                // kod buraya geldiyse tamsayi disinda bir deger girilmistir
+                girilenHarf = scanner.nextLine();
+
+                if (!girilenHarf.equalsIgnoreCase("Q")){
+                    System.out.println("Ya tamsayi gir, ya da bitirmek icin Q'ya bas");
+                }
+            }
+
+
+        }
+
+        System.out.println("Girilen " + sayiAdedi + " tamsayinin toplami : " + sayilarinToplami);
+
+
+    }
+}
