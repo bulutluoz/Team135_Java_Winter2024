d58f2baabe7185fee72bccb64faea034f005740e
bulutluoz <abbulutluoz@gmail.com>
2024-03-04 10:39:58 +0100

    day32 sonu

---
+++ src/day32_maps/C04_SinifOgrenciListesi.java
@@ -0,0 +1,10 @@
+package day32_maps;
+
+public class C04_SinifOgrenciListesi {
+
+    public static void main(String[] args) {
+
+        // verilen siniftaki tum ogrencilerin
+        // numara isim soyisim ve subelerini yazdirin
+    }
+}
