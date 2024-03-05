eac81314ae5d6a15bcbb19d0c4a6c92e24c56d8f
bulutluoz <abbulutluoz@gmail.com>
2024-03-05 10:46:58 +0100

    day33 sonu

---
+++ src/day33_maps/C06_FarkliDataTurleriIleElemanEkleme.java
@@ -0,0 +1,33 @@
+package day33_maps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C06_FarkliDataTurleriIleElemanEkleme {
+
+    public static void main(String[] args) {
+
+        List<Object> liste = new ArrayList<>();
+
+        liste.add("Ali");
+        liste.add(24);
+        liste.add(true);
+        liste.add('a');
+
+        int[] arr = {4,5,6,7};
+
+        liste.add(arr);
+
+
+        // listedeki arr'in uzunlugunu yazdirin
+
+
+        // Listedeki Ali'yi buyuk harf olarak yazdirin
+
+
+        // Listedeki 24'un karesini yazdirin
+
+
+
+    }
+}
