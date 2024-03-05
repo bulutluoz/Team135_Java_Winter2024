76043f4f9dd03ef0602ae18b0987eec23d3701cf
bulutluoz <abbulutluoz@gmail.com>
2024-03-05 09:44:04 +0100

    day33 sonu

--- src/day33_maps/C02_EntryIleSoyadiUpdate.java
+++ src/day33_maps/C02_EntryIleSoyadiUpdate.java
@@ -20,6 +20,32 @@ public class C02_EntryIleSoyadiUpdate {
 
         // 2. tum entry'lere ulasmak icin bir for-each loop olusturalim
 
+        for (Map.Entry<Integer,String>  eachEntry: ogrenciEntrySeti
+             ) {
 
+            // 3- value'yu direk bir array olarak kaydedelim
+            String[] eachValueArr = eachEntry.getValue().split("-"); // [Ali, Can, 11, H, MF]
+
+            // 4- istedigimiz update'i yapalim
+            eachValueArr[1] = eachValueArr[1].toUpperCase();
+
+            // 5- Array'i value olacak sekilde String'e donusturelim ve entry'i update edelim
+
+            eachEntry.setValue( String.join("-",eachValueArr)   );
+
+        }
+
+        System.out.println(ogrenciMap);
+        /*
+                {
+                101=Ali-CAN-11-H-MF,
+                102=Veli-CEM-10-K-TM,
+                103=Ali-CEM-11-K-TM,
+                104=Ayse-CAN-10-H-MF,
+                105=Sevgi-CEM-11-M-TM,
+                106=Sevgi-CAN-10-K-MF,
+                107=Esra-HAN-11-M-SOZ
+                }
+         */
     }
 }
